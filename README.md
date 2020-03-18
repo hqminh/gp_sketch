812b2564f01d4b6b8d245a5fc28f906a4ff2c385
Hai <htpham@andrew.cmu.edu>
2020-03-18 17:06:31 -0400

    update plot min-eigen-vals vs. epochs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# gp_sketch
+# gp_sketch run as follow on CPU: 
+`CUDA_VISIBLE_DEVICES='' python gp_sketch.py`
+
+Change the parameters in the constants section of the file above to customize
+ the run. 
+ 
--- gp_sketch.py
+++ gp_sketch.py
@@ -27,9 +27,10 @@ if __name__ == '__main__':
     # --- CONSTANSTS ----
     n_data = 2000
     n_test = 100
-    n_dim = 5
-    n_iter = 50
+    n_dim = 200
+    n_iter = 2
     tolerance = 0.1
+    lr = 0.2
 
     # --- DATA ----
     # noise = 0.1
@@ -45,9 +46,10 @@ if __name__ == '__main__':
     n_dim = 8
 
     # --- TRAINING ----
+    min_eigens = []
     gp = GP(data)
     model = nn.ModuleList([gp.cov, gp.mean])
-    optimizer = opt.Adam(model.parameters())
+    optimizer = opt.Adam(model.parameters(), lr=lr)
     for i in range(n_iter + 1):
         model.train()
         optimizer.zero_grad()
@@ -56,46 +58,94 @@ if __name__ == '__main__':
         if i < n_iter + 1:
             loss.backward()
             optimizer.step()
+        min_eig_value = get_min_eigen_val(gp).data.numpy()
+        print(min_eig_value)
+        min_eigens.append(np.abs(min_eig_value))
+
+
+        if (i + 1) % 10 == 0:
+            # --- PREDICTION ----
+            sgp = SGP(data)
+            sgp.cov.weights = gp.cov.weights
+            sgp.cov.sn = gp.cov.sn
+            sgp.mean.mean = gp.mean.mean
+
+            sgp_pred = sgp(test['X'], tolerance)
+            gp_pred = gp(test['X'])[0]
+
+            print(rmse(sgp_pred, test['Y']), rmse(gp_pred, test['Y']))
+            print("Diff between GP and SGP: {}".format(rmse(sgp_pred, gp_pred)))
+
+            min_eig_value = get_min_eigen_val(gp)
+            print("min_eig_value={}, threshhold={} "
+                  .format(min_eig_value,
+                          tolerance / (1. - tolerance) / min_eig_value)
+                  )
+
+            # --- PLOT ----
+            rcParams['figure.figsize'] = 16, 9
+            solutions_diff = torch.abs(sgp_pred - gp_pred).data.numpy()
+            plt.close()
+            plt.plot(range(n_test), solutions_diff,
+                     '-o',
+                     color='b', lw=1.5,
+                     label='|SGP-GP|')
+
+            plt.ylabel('diffirence bw sketched GP and GP', fontsize=24)
+            plt.xlabel('test points', fontsize=24)
+            plt.xticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='x')
+            plt.yticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='y')
+            plt.legend(fontsize=17.5,
+                       # loc=(1.04, 0)
+                       )
+            plt.grid()
+            plt.tight_layout()
+            # plt.title('GP vs SGP', fontsize=30)
+            plt.savefig('sgp_diff_gp_iter{}.pdf'.format(i+1))
+
+            plt.show()
+
+            # eigen
+            plt.close()
+            plt.plot(range(n_iter + 1), min_eigens, '-d', color='r', lw=1.5,
+                     # label='min_eigen_vals'
+                     )
+            plt.ylabel('minimum eigen values', fontsize=24)
+            plt.xlabel('iters', fontsize=24)
+            plt.xticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='x')
+            plt.yticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='y')
+            plt.legend(fontsize=17.5,
+                       # loc=(1.04, 0)
+                       )
+            plt.grid()
+            plt.tight_layout()
+            # plt.title('GP vs SGP', fontsize=30)
+            plt.savefig('min_eigen_iter{}.pdf'.format(i+1))
+            plt.show()
+
+            plt.close()
+            plt.plot(range(n_iter + 1), np.log(np.array(min_eigens)), '-x',
+                     color='g', lw=1.5,
+                     # label='min_eigen_vals'
+                     )
+            plt.ylabel('minimum eigen values', fontsize=24)
+            plt.xlabel('iters', fontsize=24)
+            plt.xticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='x')
+            plt.yticks(fontsize=18)
+            # plt.locator_params(nbins=11, axis='y')
+            # plt.legend(fontsize=17.5,
+            # loc=(1.04, 0)
+            # )
+            plt.grid()
+            plt.tight_layout()
+            # plt.title('GP vs SGP', fontsize=30)
+            plt.savefig('log_min_eigen_iter{}.pdf'.format(i+1))
+            plt.show()
+
     print('Done')
 
-    # --- PREDICTION ----
-    sgp = SGP(data)
-    sgp.cov.weights = gp.cov.weights
-    sgp.cov.sn = gp.cov.sn
-    sgp.mean.mean = gp.mean.mean
-
-    sgp_pred = sgp(test['X'], tolerance)
-    gp_pred = gp(test['X'])[0]
-
-    print(rmse(sgp_pred, test['Y']), rmse(gp_pred, test['Y']))
-    print("Diff between GP and SGP: {}".format(rmse(sgp_pred, gp_pred)))
-
-    min_eig_value = get_min_eigen_val(gp)
-    print("min_eig_value={}, threshhold={} "
-          .format(min_eig_value,
-                  tolerance / (1. - tolerance) / min_eig_value)
-          )
-
-    # --- PLOT ----
-    rcParams['figure.figsize'] = 16, 9
-    solutions_diff = torch.abs(sgp_pred - gp_pred).data.numpy()
-    plt.close()
-    plt.plot(range(n_test), solutions_diff,
-             '-o',
-             color='b', lw=1.5,
-             label='|SGP-GP|')
-
-    plt.ylabel('diffirence bw sketched GP and GP', fontsize=24)
-    plt.xlabel('test points', fontsize=24)
-    plt.xticks(fontsize=18)
-    # plt.locator_params(nbins=11, axis='x')
-    plt.yticks(fontsize=18)
-    # plt.locator_params(nbins=11, axis='y')
-    plt.legend(fontsize=17.5,
-               # loc=(1.04, 0)
-               )
-    plt.grid()
-    plt.tight_layout()
-    # plt.title('GP vs SGP', fontsize=30)
-    plt.savefig('sgp_diff_gp.pdf')
-    plt.show()
